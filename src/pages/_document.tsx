d462c93b51917e6b219fb54b8ae65f895d55da91
Ricardo <ricardo10_2010@hotmail.com>
2021-06-04 02:05:42 -0300

    fixing name

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,6 @@
-import Document, {Html, Head, Main, NextScript} from 'next/document';
+import document, {Html, Head, Main, NextScript} from 'next/document';
 
-export default class MyDocument extends Document {
+export default class MyDocument extends document {
     render() {
         return (
             <Html>
